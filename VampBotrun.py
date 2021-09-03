b5c3fe48c9b70cc295d950c560ee6895044826e8
MICKEY♡ <88245294+D15H4NT0P@users.noreply.github.com>
2021-09-03 15:26:09 +0530

    Update and rename lightningrun.py to VampBotrun.py

---
+++ VampBotrun.py
@@ -0,0 +1,2 @@
+import os
+os.system("git clone https://github.com/D15H4NT0P/VAMPUSERBOT/root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
--- lightningrun.py
+++
@@ -1,2 +0,0 @@
-import os
-os.system("git clone https://github.com/D15H4NT0P/VAMPUSERBOT /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
